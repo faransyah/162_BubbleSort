62bb13a9673c24e3a44036516fda5e881222a026
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-03-17 16:16:07 +0700

    massukan
    int a[20];                  // Deklarasi array a dengan ukuran 20
    int n;                     // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -1,20 +1,60 @@
-// BubbleSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int a[20];                  // Deklarasi array a dengan ukuran 20
+int n;                     // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+
+void input() {           // Procedure untuk input
+    while (true) {       // looping 
+        cout << "Masukkan banyaknya elemen pada : "; // output ke layar
+        cin >> n;        // Input dari pengguna 
+        if (n <= 20)     // Jika n kurang dari atau sama dengan 20
+            break;      // Keluar dari loop 
+        else {           // Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // Output ke layar
+        }
+    }
+    cout << endl;                                  // Output baris kosong
+    cout << "====================" << endl;        // Output ke layar
+    cout << "Masukkan Elemen Array" << endl;       // Output ke layar
+    cout << "====================" << endl;        // Output ke layar
+
+    for (int i = 0; i < n; i++) {  // Looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << "}"; // Output ke layar 
+        cin >> a[i];                    // Input dari pengguna
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void display() {
+    cout << endl;                               // Output baris kosong
+    cout << "==================================" << endl;   // Output ke layar 
+    cout << "Element Array yang telah tersusun" << endl;         // Output ke layar
+    cout << "==================================" << endl;   // OUtput ke layar 
+    for (int j = 0; j < n; j++) {
+        cout << a[j] << endl;
+    }
+    cout << endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void bubbleSortArray() {
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j = 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
+
+int main() {
+    input();
+    bubbleSortArray();
+    display(); 
+    return 0; 
+}
